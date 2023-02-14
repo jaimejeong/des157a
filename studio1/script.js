700c66de20d8c8b1f9e94194df1d00c5aa9f3590
Jaime Jeong <jeong20@scc2101m-12.ucdavis.edu>
2023-02-13 21:33:52 -0800

    work from computer lab

--- studio1/script.js
+++ studio1/script.js
@@ -1,3 +1,43 @@
 (function() {
-    
-})
+    "use strict";
+    console.log('reading js');
+
+    const myForm = document.querySelector('#myForm');
+    const madlib = document.querySelector('#madlib');
+
+    myForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const base = document.querySelector('#base').value;
+        const color = document.querySelector('#color').value;
+        const adjective = document.querySelector('#adjective').value;
+        const fruit = document.querySelector('#fruit').value;
+        const noun = document.querySelector('#noun').value;
+
+
+        // const myText = `base: ${base} color: ${color}`;
+        let myText;
+
+        myText = `The cocktail base you choose is ${base}`;
+        if (base == 'gin') {
+            myText += `Let's make a classic martini`;
+        }
+        else if (base == 'vodka') {
+            myText = "vodka is nasty";
+        }
+        madlib.innerHTML = myText;
+    })
+
+    // const submitButton = document.querySelectorAll('input[type=submit]');
+
+    document.querySelector('#send-form').addEventListener('click', function(event) {
+        event.preventDefault;
+        document.getElementById('output').className = 'showing';
+    } )
+
+    document.querySelector('#send-form').addEventListener('click', function(event) {
+        event.preventDefault;
+        document.getElementById('input').className = 'hidden';
+    })
+
+}())
