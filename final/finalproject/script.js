4f2b856e683018c7aaef83f8ffeca1beb88a4806
Jaime Jeong <jaimejeong20@gmail.com>
2023-03-22 15:53:18 -0700

    almost done

--- final/finalproject/script.js
+++ final/finalproject/script.js
@@ -6,11 +6,15 @@
     const articleimage=document.querySelector('#imagefull');
     const imagefull = document.querySelector('#imagefull img');
     const thumbnails = document.querySelectorAll('.thumbnail');
-    // add querySeletorAll for selected thumbnails
+    const selects = document.querySelectorAll('.select');
     const closebtn = document.querySelector('.closebtn');
+    let newheading = document.querySelector('h2');
+    let para = document.querySelector('p');
 
+    // for each area of image map
     for (const area of areas) {
 
+        // when the mouse hover 
         area.addEventListener('mouseover', function () {
             document.getElementById(`${area.id}_th`).className = 'thumbnail showing';
             document.getElementById(`${area.id}_th`).style.cursor = 'pointer';
@@ -20,30 +24,49 @@
             event.preventDefault();
             openSide();
             // hide all selected thumbnails, queryselectedall
-
+            for (const select of selects) {
+                select.className = 'select hidden';
+            }
             // show corrected selected thumbnails
-
+            document.getElementById(`${area.id}_sel`).className = 'select showing';
             // document.getElementById(`${event.target.id}_th`).className = 'thumbnail showing';
            
             // show full image
             imagefull.src = `images/${area.id}_full.png`;
             articleimage.className='showing';
 
-            // add image with selected state, change id
-            // document.getElementById(`${area.id}_sel`).className = 'thumbnail showing';
-            // hide thumbnail, thumbnail hidden
+            let currentid = `${area.id}_sel`;
 
+
+            if (currentid == 'grandpa_sel') {
+                newheading.innerHTML = 'Grandparents from Busan, South Korea';
+                para.innerHTML = "took the train with my mom and visited my grandparents for grandpa's birthday";
+            }
+
+            else if (currentid == 'watertower_sel') {
+                newheading.innerHTML = 'Watertower on campus';
+                para.innerHTML = "this picture is from last winter quarter";
+            }
+
+            // add image with selected state, change id
+            // document.getElementById(`${area.id}_sel`).className = 'select showing';
+            // hide thumbnail, thumbnail hidden
+            // document.querySelectorAll(`${area.id}_sel`).className = 'select hidden';
         });
 
         document.getElementById(`${area.id}_th`).addEventListener('mouseout', function() {
             document.getElementById(`${area.id}_th`).className = 'thumbnail hidden';
         })
 
+        // exiting when clicking the close button
         closebtn.addEventListener('click', function(event) {
             event.preventDefault();
             closeSide();
             // close selected thumbnail
-
+            for (const select of selects) {
+                select.className = 'select hidden';
+            }
+            // document.querySelectorAll(`${area.id}_sel`).className = 'select hidden';
             // document.getElementById(`${area.id}_th`).className = 'thumbnail hidden';
 
         })
